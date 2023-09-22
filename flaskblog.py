db96df42d5e394c4821476373b76d9430c3a379e
alex-njuguna <njugunakinuthia013@gmail.com>
2023-09-22 13:23:55 +0300

    template setup

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
+posts = [
+    {
+    'title': 'The last of the mohicans',
+    'author': 'steven spelberg',
+    'date_posted': 'Jan 8, 1995',
+    'content': 'First post content'
+},
+{
+    'title': 'Facing mount Kenya',
+    'author': 'Jomo Kenyatta',
+    'date_posted': 'March 8, 1943',
+    'content': 'Second post content'
+}
+]
 @app.route("/home")
 @app.route("/")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
