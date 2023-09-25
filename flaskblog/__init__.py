1bfc3da69036bf2b4c30badba74db7e18004c672
alex-njuguna <njugunakinuthia013@gmail.com>
2023-09-25 19:05:34 +0300

    handling redirection

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 
@@ -10,7 +12,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
 
 from flaskblog import app, db
 from flaskblog.forms import RegistrationForm, LoginForm
@@ -33,6 +34,9 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    
     form = RegistrationForm()
 
     if form.validate_on_submit():
@@ -54,16 +58,33 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
     form = LoginForm()
     if form.validate_on_submit():
         email = form.email.data
         user = User.query.filter_by(email=email).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
                 flash("Signed in successfully!", "success")
-                return redirect(url_for("home"))
+                next_page = request.args.get("next")
+                return redirect(next_page) if next_page else redirect(url_for("home"))
             else:
                 flash("Incorrect details!", "danger")
         else:
             flash("User does not exist!", "danger")
     return render_template("login.html", title="login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="account")
+
