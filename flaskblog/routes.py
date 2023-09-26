b2cbd6808456b47beac6aeb32b59b3b7e9464676
alex-njuguna <njugunakinuthia013@gmail.com>
2023-09-27 01:13:04 +0300

    routing to a post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -117,3 +117,9 @@ def new_post():
         flash(f"{form.title.data.title()} created", "success")
         return redirect(url_for("home"))
     return render_template("create_post.html", title="new post", form=form)
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
