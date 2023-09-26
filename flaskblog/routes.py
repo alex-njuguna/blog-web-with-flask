2c7e79fd5258f7f465c5719ea0645f82158bee30
alex-njuguna <njugunakinuthia013@gmail.com>
2023-09-27 01:41:12 +0300

    post update

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 
 from flaskblog import app, db
@@ -123,3 +123,23 @@ def new_post():
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template("post.html", title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data.title()
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated", "success")
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title="update post", legend="Update Post", form=form)
+
