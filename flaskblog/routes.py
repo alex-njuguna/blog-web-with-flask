d277edbffb2528f0c952b0728a5bd09d37b77b36
alex-njuguna <njugunakinuthia013@gmail.com>
2023-10-06 15:24:01 +0300

    sorting apgination and arranging posts from the latest to the oldest

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -13,7 +13,8 @@ from flaskblog import bcrypt
 @app.route("/home")
 @app.route("/")
 def home():
-    posts = Post.query.all()
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template("home.html", posts=posts)
 
 
